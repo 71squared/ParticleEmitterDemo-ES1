195abcb3b09f9485a613ac55317314b5afaa0767
Tom Bradley <tom@71squared.com>
2014-02-01 17:36:31 +0000

    Initial Commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ParticleEmitterDemo
+//
+//  Created by Mike Daley on 12/05/2010.
+//  Copyright 71Squared 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
